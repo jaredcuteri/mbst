9153d73ff8cb2fc0c27deb7adad251d7016f5cc5
Jared Cuteri <jaredcuteri@gmail.com>
2019-05-19 03:24:38 +0100

    animation test

--- AccelSetupTest.py
+++ AccelSetupTest.py
@@ -2,25 +2,63 @@ import board
 import busio
 from ImuPalace import Adxl345
 from ImuPalace import Mma8451
+from datetime import datetime
 import time
+
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib import style
 
 mpss2g = 1/9.81
 
-# TODO: Make Homogenous wrapper class that creates common interface for MMA and ADXL
+# Instantiate sensors
+numSamp = 40
 sensors = {}
+accelHist = {}
+timeHist = [0] * numSamp
 time.sleep(0.2) # Sleep necessary to avoid I/O error (Why?)
 sensors['mma'] = Mma8451.Mma8451()
+accelHist['mma'] = [(0, 0, 0), ] * numSamp
 time.sleep(0.2)
 sensors['adxl'] = Adxl345.Adxl345()
+accelHist['adxl'] = [(0, 0, 0), ] * numSamp
 
+# Configure sensor settings
 for sensor in sensors.values():
     sensor.setRange(8)
     sensor.setDataRate(800)
 
+# Set up plot
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+style.use('fivethirtyeight')
+
+# Save off data collection start time
+start_time = datetime.now()
+
+ln, = plt.plot([], [], 'ro')
+
+def init():
+    return ln,    
+def animate(i):
+    # TODO: instead of pop/append use queue
+    samp_time = datetime.now()
+    delt_time = samp_time - start_time
+    time_s = delt_time.seconds + (delt_time.microseconds)*1e-6
+    timeHist.pop()
+    timeHist.append(time_s)
+
+    for name, sensor in sensors.items():
+        _ = accelHist[name].pop()
+        accelHist[name].append(sensor.getAccels())
+    
+    ax1.clear()
+    ln.set_data(timeHist,list([accels[2] for accels in accelHist['adxl']]))
+    return ln,
+ani = animation.FuncAnimation(fig, animate,init_func=init, blit=True, interval=500)
+Writer = animation.writers['html']
+writer = Writer(fps=2, metadata=dict(artist='Me'), bitrate=1800)
+ani.save('test.html',writer=writer)
 
 while True:
-   # xg_adxl, yg_adxl, zg_adxl = tuple([mpss2g*acc for acc in (x_adxl, y_adxl, z_adxl)])i
-    for name, sensor in sensors.items():
-        xg, yg, zg = sensor.getAccels()
-        print('{0}  Ax={1:6.3f} Ay={2:6.3f} Az={3:6.3f}'.format(name, xg, yg, zg))    
     time.sleep(0.5)

da3def747684aa46dc0068a9cf8b3b610e9f4651
Jared Cuteri <jaredcuteri@gmail.com>
2019-05-19 17:39:04 -0700

    Human interface setup, record button, and status LEDs

--- AccelSetupTest.py
+++ AccelSetupTest.py
@@ -5,48 +5,83 @@ from datetime import datetime
 from ImuPalace import Adxl345
 from ImuPalace import Mma8451
 import Logger
+import RPi.GPIO as GPIO
 import time
-
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-from matplotlib import style
 
 mpss2g = 1/9.81
 dataPrecision = 6 
+
+# GPIO Pin Setup
+runningPin = 21
+recordPin = 20
+recSwitchPin = 16
+GPIO.setup(runningPin, GPIO.OUT) # Code Running LED
+GPIO.setup(recordPin, GPIO.OUT) # Data Recording LED
+GPIO.setup(recSwitchPin, GPIO.IN)
+
+codeRunning = True
+dataRecording = False
+GPIO.output(runningPin, codeRunning)
+GPIO.output(recordPin, dataRecording)
+
 # Necessary to ensure data is output in correct order
 sensors = OrderedDict()
 # Instantiate sensors
 sensors['MMA8451'] = Mma8451.Mma8451()
 sensors['ADXL345'] = Adxl345.Adxl345()
-dataRate = 400 #Hz
+dataRate = 200 #Hz
 # Configure sensor settings
 for sensor in sensors.values():
     sensor.setRange(8)
     sensor.setDataRate(dataRate)
 
-# Save off data collection start time
-startTime = datetime.now()
 
 # Log File Setup
-dataLog = Logger.Logger(filename="AccelData")
 AccelConfigStr = ""
 DataHeaderStr = "t[s] "
 for idx, name in enumerate(sensors.keys()):
     AccelConfigStr += "Accel#"+str(idx)+" "+name+" "
     DataHeaderStr += "Ax"+str(idx)+"[g] Ay"+str(idx)+"[g] Az"+str(idx)+"[g] "
-dataLog.writeString(AccelConfigStr+"\n")
-dataLog.writeHeader(DataHeaderStr.split(" "))
-dataSamples = 0
+firstPassRec = True
+firstPassOff = True
+recordData = False
+# Add power switch
 while True:
-    currTime = datetime.now()
-    relTime = currTime-startTime
-    dataOut = [relTime.total_seconds(),]
-    for name, sensor in sensors.items():
-        dataOut += sensor.getAccels()    
-    dataOut = [ round(datum,dataPrecision) for datum in dataOut ]
-    dataLog.writeData(dataOut)
-    dataSamples += 1
-    print( "Data Samples Collected: "+str(dataSamples), end='\r')
-    # Save Accels to file
-    # Getting about 40 Hz with sleep
-    #time.sleep(1/dataRate)
+    if recordData:
+        if firstPassRec:
+            firstPassOff = True
+            dataRecording = True
+            GPIO.output(recordPin, dataRecording)
+            dataLog = Logger.Logger(filename="AccelData",filepath="../mbst_data/")
+            dataLog.writeString(AccelConfigStr+"\n")
+            dataLog.writeHeader(DataHeaderStr.split(" "))
+            print('',end='\n')
+            print('Writing Output to: ' + dataLog.filename)
+            # Save off data collection start time
+            startTime = datetime.now()
+            dataSamples = 0
+            firstPassRec = False
+            
+        currTime = datetime.now()
+        relTime = currTime-startTime
+        dataOut = [relTime.total_seconds(),]
+        for name, sensor in sensors.items():
+            dataOut += sensor.getAccels()    
+        dataOut = [ round(datum,dataPrecision) for datum in dataOut ]
+        dataLog.writeData(dataOut)
+        dataSamples += 1
+        print( "Data Samples Collected: "+str(dataSamples), end='\r')
+    else:
+        # Check Switch
+        if firstPassOff:
+            firstPassRec = True
+            dataRecording = False
+            GPIO.output(recordPin, dataRecording)
+            firstPassOff = False
+        
+    if GPIO.input(recSwitchPin):
+        recordData = not recordData
+        time.sleep(1)
+
+codeRunning = False
+GPIO.output(runningPin, codeRunning)
--- Logger.py
+++ Logger.py
@@ -1,17 +1,21 @@
 import csv
 import datetime
+import os
+import os.path as path
 
 class Logger():
     
-    startTime = datetime.datetime.now() 
-    startTimeAbrv = startTime.strftime("_%Y%m%d_%H%M%S")
-    startTimeStr = startTime.strftime("%Y-%m-%d %H:%M:%S")
-    
-    def __init__(self, filename="DataLog",desc="Data Log"):
-        self.logFile = open(filename+Logger.startTimeAbrv+".csv","w")
+    def __init__(self, filename="DataLog",filepath=".", desc="Data Log"):
+        startTime = datetime.datetime.now() 
+        startTimeAbrv = startTime.strftime("_%Y%m%d_%H%M%S")
+        startTimeStr = startTime.strftime("%Y-%m-%d %H:%M:%S")
+        self.filename = path.join(filepath, filename+startTimeAbrv+".csv")
+        if not path.isdir(filepath):
+            os.mkdir(filepath)
+        self.logFile = open(self.filename,"w")
         self.logWriter = csv.writer(self.logFile, delimiter=',')
         self.logFile.write(desc + "\n")
-        self.logFile.write("Recording Started at: " + Logger.startTimeStr+"\n")
+        self.logFile.write("Recording Started at: " + startTimeStr+"\n")
     
     def writeString(self,string):
         self.logFile.write(string)
